ed3b6636e5e9713e6e041bc06e6e1c1331c09dae
bernard <jcshen1230@126.com>
2020-09-24 17:59:19 +0800

    add test bw

---
+++ e4/bw_test.c
@@ -0,0 +1,83 @@
+#include "common.h"
+#include <pthread.h>
+
+// struct config_t config = {
+//     NULL,
+//     "10.10.10.1",
+//     19875,
+//     1,
+//     NULL,
+//     2
+// };
+
+struct config_t config = {
+    NULL,
+    NULL, 
+    19875,
+    1,
+    NULL, 
+    2
+};
+
+struct thread_arg {
+    struct resources *res;
+    int qpn;
+};
+
+void * thread_run(void *arg) {
+    struct resources *res = ((struct thread_arg*)arg)->res;
+    int qpid = ((struct thread_arg*)arg)->qpn;
+    unsigned long st;
+    unsigned long et;
+    unsigned long spent = 0;
+    struct timeval cur_time;
+    printf("Thread %d created\n", qpid);
+    if (config.server_name) {
+        for (int i = 0; i < 10; i ++) {
+            gettimeofday(&cur_time, NULL);
+            st = (cur_time.tv_sec * 1000) + (cur_time.tv_usec / 1000);
+            post_send(res, IBV_WR_RDMA_WRITE, qpid);
+            poll_completion(res, qpid);
+            gettimeofday(&cur_time, NULL);
+            et = (cur_time.tv_usec * 1000) + (cur_time.tv_usec / 1000);
+            spent += (et - st);
+        }
+    }
+    unsigned long *retval = (unsigned long *)malloc(sizeof(unsigned long));
+    *retval = spent;
+    return retval;
+}
+
+void main() {
+    struct resources res;
+    char tempChar;
+    unsigned long **time_spents;
+    pthread_t *ths;
+    
+    time_spents = (unsigned long **)malloc(config.num_qp * sizeof(unsigned long *));
+    ths = (pthread_t *)malloc(config.num_qp * sizeof(pthread_t));
+
+    resources_init(&res);
+    resources_create(&res);
+    connect_qp(&res);
+
+    for (int i = 0; i < config.num_qp; i++) {
+        int ret;
+        struct thread_arg targ = {
+            .qpn = i, .res = &res
+        };
+        ret = pthread_create(&ths[i], NULL, thread_run, &targ);
+        if (ret) {
+            fprintf(stderr, "Failed to create thread %d\n", i);
+            return;
+        }
+    }
+    for (int i = 0; i < config.num_qp; i++) {
+        pthread_join(ths[i], (void **)&time_spents[i]);
+    }
+
+    for (int i = 0; i < config.num_qp; i++) {
+        printf("%d: %lu\n", i, *time_spents[i]);
+    }
+    return;
+}
---
+++ e4/common.h
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <endian.h>
+#include <byteswap.h>
+#include <getopt.h>
+
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <infiniband/verbs.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define BUF_SIZE 65536
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+static inline uint64_t htonll(uint64_t x) { return bswap_64(x); }
+static inline uint64_t ntohll(uint64_t x) { return bswap_64(x); }
+#elif __BYTE_ORDER == __BIG_ENDIAN
+static inline uint64_t htonll(uint64_t x) { return x; }
+static inline uint64_t ntohll(uint64_t x) { return x; }
+#else
+#error __BYTE_ORDER is neither __LITTLE_ENDIAN nor __BIG_ENDIAN
+#endif
+
+struct config_t
+{
+	const char *dev_name; /* IB device name */
+	char *server_name;	/* server host name */
+	u_int32_t tcp_port;   /* server TCP port */
+	int ib_port;		  /* local IB port to work with */
+	int gid_idx;		  /* gid index to use */
+    int num_qp;         // number of qp to create
+};
+
+struct cm_con_data_t
+{
+	uint64_t addr;   /* Buffer address */
+	uint32_t rkey;   /* Remote key */
+	uint32_t qp_num; /* QP number */
+	uint16_t lid;	/* LID of the IB port */
+	uint8_t gid[16]; /* gid */
+} __attribute__((packed));
+
+struct resources
+{
+	struct ibv_device_attr device_attr;
+	/* Device attributes */
+	struct ibv_port_attr port_attr;	/* IB port attributes */
+	struct cm_con_data_t **remote_props; /* values to connect to remote side */
+	struct ibv_context *ib_ctx;		   /* device handle */
+	struct ibv_pd *pd;				   /* PD handle */
+	struct ibv_cq **cq;				   /* CQ handle */
+	struct ibv_qp **qp;				   /* QP handle */
+	struct ibv_mr **mr;				   /* MR handle for buf */
+	char **buf;						   /* memory buffer pointer, used for RDMA and send
+ops */
+	int sock;						   /* TCP socket file descriptor */
+};
+
+int sock_connect(const char *serverName, int port);
+void resources_init(struct resources *res);
+int resources_create(struct resources *res);
+int modify_qp_to_init(struct ibv_qp *qp);
+int modify_qp_to_rtr(struct ibv_qp *qp, uint32_t remoteQPN, uint16_t dlid, uint8_t *dgid);
+int modify_qp_to_rts(struct ibv_qp *qp);
+int sock_sync_data(int sock, int xfer_size, char *localData, char *remoteData);
+int connect_qp(struct resources *res);
+int post_send(struct resources *res, int opcode, int qpid);
+int post_receive(struct resources *res, int qpid);
+int poll_completion(struct resources *res, int qpid);
